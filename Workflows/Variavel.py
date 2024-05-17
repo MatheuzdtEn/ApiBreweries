8d577e53f5eb2fcbce52566e1a9d2782c4edf3d6
MatheuzdtEn <MatheuzdtEn>
2024-05-17 21:09:27 +0000

    FINAL
    FINAL

--- Workflows/Variavel.py
+++ Workflows/Variavel.py
@@ -1,6 +1,9 @@
 # Databricks notebook source
 #Host_url
-base_url = "<<Seu_Host_Url>>"
+base_url = "<<SUA_URL_AQUI>>"
 
-# Token de acesso
-token = "<<Seu_Token>"
+# Token de acesso: Definicôes --> Programador --> Gerar Token
+token = "<<SEU_TOKEN_AQUI>>"
+
+# Seu email, certifique de estar registrado no ambiente. 
+email = "<<SEU_EMAIL_AQUI>>"
