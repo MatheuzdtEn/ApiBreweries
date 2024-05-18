399fb98e6ac40341ace83dd47cc8476558dc3776
Matheus Gustavo lopes dos Reis <106482156+MatheuzdtEn@users.noreply.github.com>
2024-05-18 10:52:00 -0300

    Update Create_Workflow.py

--- Databricks_Projeto_1/Workflows/Create_Workflow.py
+++ Databricks_Projeto_1/Workflows/Create_Workflow.py
@@ -1,8 +1,39 @@
-# Databricks notebook source
 import json
 import requests
 
 def create_job(username, token, base_url):
+    # Função para obter o ID do cluster
+    def get_cluster_id():
+        # Endpoint para listar os clusters
+        list_clusters_url = f"{base_url}/api/2.0/clusters/list"
+        
+        # Cabeçalho de autenticação
+        headers = {
+            "Authorization": f"Bearer {token}"
+        }
+
+        # Requisição para listar os clusters
+        response = requests.get(list_clusters_url, headers=headers)
+
+        # Verificar a resposta
+        if response.status_code == 200:
+            clusters = response.json().get('clusters', [])
+            if clusters:
+                # Retornar o ID do primeiro cluster na lista (ou ajustar conforme necessário)
+                return clusters[0].get('cluster_id')
+            else:
+                print("Nenhum cluster encontrado.")
+                return None
+        else:
+            print(f"Erro ao listar os clusters: {response.status_code} - {response.text}")
+            return None
+
+    # Obter o ID do cluster
+    cluster_id = get_cluster_id()
+    if not cluster_id:
+        print("Não foi possível obter o ID do cluster.")
+        return None
+
     # JSON definido como uma string e atribuído a uma variável
     json_config = """
     {
@@ -26,7 +57,7 @@ def create_job(username, token, base_url):
             "notebook_path": "Databricks_Projeto_1/Notebooks/ApiBreweries/Pipeline__ApiBreweries",
             "source": "GIT"
           },
-          "existing_cluster_id": "0516-195718-ycordw6n",
+          "existing_cluster_id": "{cluster_id}",
           "max_retries": 2,
           "min_retry_interval_millis": 0,
           "retry_on_timeout": false,
@@ -57,8 +88,8 @@ def create_job(username, token, base_url):
       }
     }
     """
-    # Substituir placeholders no JSON pelo nome de usuário
-    json_config = json_config.replace("{username}", username)
+    # Substituir placeholders no JSON pelo nome de usuário e ID do cluster
+    json_config = json_config.replace("{username}", username).replace("{cluster_id}", cluster_id)
 
     # Carregar a string JSON em um dicionário Python
     job_config = json.loads(json_config)
@@ -91,4 +122,3 @@ def create_job(username, token, base_url):
         print(f"Erro ao criar o job: {response.status_code} - {response.text}")
 
     return response.json() if response.status_code == 200 else None
-
